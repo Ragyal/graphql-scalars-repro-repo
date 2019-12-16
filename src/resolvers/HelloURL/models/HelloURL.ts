a6e3270ccc3eb03076c31b1d5e37baecdac9d355
Ove von Stackelberg <26764163+Ragyal@users.noreply.github.com>
2019-12-16 23:56:01 +0100

    Version 4 (working)

--- src/resolvers/HelloURL/models/HelloURL.ts
+++ src/resolvers/HelloURL/models/HelloURL.ts
@@ -1,9 +1,9 @@
 import { Field, ObjectType } from "type-graphql";
-// import { URLResolver } from "graphql-scalars";
+import { URLResolver } from "graphql-scalars";
 
 @ObjectType()
 export class HelloURL {
-  @Field(() => String, { nullable: true })
+  @Field(() => URLResolver, { nullable: true })
   url: string | undefined;
 
   @Field(() => String)
